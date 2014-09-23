6037c30bb9cec3843de08e82ce6291b25884ddac
Kaifei Chen <kaifei@berkeley.edu>
2014-09-22 23:39:22 -0700

    Add convert script to convert data to csv file

---
+++ analysis/ml/convert.py
@@ -0,0 +1,45 @@
+from glob import glob
+import os
+
+datadir = "./data/"
+output_fname = "data.csv"
+
+
+# get all files' full paths
+fpaths = []
+for root, subFolders, files in os.walk(datadir):
+    for fname in files:
+        if fname.endswith("csv") and "metadata" not in fname:
+            fpath = os.path.join(root, fname)
+            fpaths.append(fpath)
+
+# parse them
+macs = [] # list of MAC addresses
+data = {} # dict with timestamp as keys, with dict as values
+for fpath in fpaths:
+    print "handling", fpath
+    with open(fpath) as f:
+        f.readline() # skip header
+        lines = f.readlines()
+    records = [l.strip().split(",") for l in lines]
+    last_timestamp = 0
+    for r in records:
+        timestamp = int(r[0])
+        # validate timestamp
+        if 0 < timestamp - last_timestamp < 5:
+            raise Exception("timestamp error " + str(timestamp) + " " + str(last_timestamp))
+        last_timestamp = timestamp
+        mac = r[3]
+        rssi = int(r[5])
+        macs.append(mac)
+        if timestamp not in data:
+            data[timestamp] = {}
+        data[timestamp][mac] = rssi
+macs = list(set(macs))
+
+# write to output file
+with open(output_fname, "w") as f:
+    f.write(",".join(macs) + "\n")
+    for items in data.values():
+        row_vals = [str(items.get(m, "?")) for m in macs]
+        f.write(",".join(row_vals) + "\n")

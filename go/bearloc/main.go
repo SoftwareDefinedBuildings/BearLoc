eee01d9d3ef8011ad4cc24621348a07217f84598
Kaifei Chen <kaifei@berkeley.edu>
2015-01-02 15:54:53 -0800

    Update go algorithm codes
    - Eliminate requesting data from sensor, only subscribe to sensor

--- go/bearloc/main.go
+++ go/bearloc/main.go
@@ -9,15 +9,24 @@ import (
   "encoding/json"
   "syscall"
   "os/signal"
-  "strconv"
 )
 
-var fakelocation string =   `{ "type": "estimated semloc", "id": "3f2cd8d0-9831-11e3-a5e2-0800200c9a66", "target id": "1d352410-4a5e-11e3-8f96-0800200c9a66", "epoch": 1387670483532, "country": "US", "state": "CA", "city": "Berkeley", "street": "Leroy Ave", "building": "Soda Hall", "locale": "494"}`
+var fakeLocation = Location{"US", "CA", "Berkeley", "Leroy Ave", "Soda Hall", "494"}
+
+type Location struct {
+    Country     string      `json:country`
+    State       string      `json:state`
+    City        string      `json:city`
+    Street      string      `json:street`
+    Building    string      `json:building`
+    Locale      string      `json:locale`
+}
 
 type LocRequest struct {
     Msgtype     string      `json:"msgtype"`
     Uuid        string      `json:"uuid"`
-    Epoch       int64         `json:"epoch"`
+    Epoch       int64       `json:"epoch"`
+    WifiTopic   string      `json:"wifitopic"`
     Backtopic   string      `json:"backtopic"`
 }
 
@@ -25,74 +34,64 @@ type LocResult struct {
     Msgtype     string      `json:"msgtype"`
     Uuid        string      `json:"uuid"`
     Epoch       int64       `json:"epoch"`
-    Result      string      `json:"result"`
+    Result      Location    `json:"result"`
 }
 
 type DataPoint struct {
     Msgtype     string      `json:"msgtype"`
     Uuid        string      `json:"uuid"`
-    Epoch       int64         `json:"epoch"`
-    Data        string      `json:"data"`
-}
-
-type DataRequest struct {
-    Msgtype     string      `json:"msgtype"`
-    Uuid        string      `json:"uuid"`
     Epoch       int64       `json:"epoch"`
-    Backtopic   string      `json:"backtopic"`
+    Data        string      `json:"data"`
 }
 
 var jobs = make(chan LocRequest , 100)
 
-var dataReceiveHandler MQTT.MessageHandler = func(client *MQTT.MqttClient, message MQTT.Message) {
+var WifiDataHandler MQTT.MessageHandler = func(client *MQTT.MqttClient, message MQTT.Message) {
+    topic := message.Topic()
     msg := message.Payload()
-    data := &DataPoint{}
+    data := DataPoint{}
     if err := json.Unmarshal(msg, &data); err != nil {
         panic(err)
     }
     fmt.Println(data)
 
-    for job, ok := <- jobs; ok; job, ok := <- jobs {
-        if (job.Epoch < data.Epoch) {
-            location := fakelocation//localize(data.data)
+    select {
+    case job := <-jobs:
+        fmt.Println(job.Epoch)
+        fmt.Println(data.Epoch)
+        fmt.Println(job.WifiTopic)
+        fmt.Println(topic)
+        if (job.Epoch < data.Epoch && job.WifiTopic == topic) {
+            location := fakeLocation//localize(data.data)
+            fmt.Println(location)
             result := LocResult {
                 "locResult",
                 "abcdefg",
                 time.Now().UnixNano() / 1000000,
                 location}
             resultjson, _ := json.Marshal(result)
+            fmt.Println(job.Backtopic)
+            fmt.Println(string(resultjson))
             mqttPublish(client, string(job.Backtopic), string(resultjson))
-            break
         }
+        fmt.Println("Handled")
+    default:
+        fmt.Println("Ignored")
     }
-    fmt.Println("Handled")
 }
 
 var locRequestHandler MQTT.MessageHandler = func(client *MQTT.MqttClient, message MQTT.Message) {
     msg := string(message.Payload())
     fmt.Println(string(msg));
-    req := &LocRequest{}
+    req := LocRequest{}
     if err := json.Unmarshal([]byte(msg), &req); err != nil {
         panic(err)
     }
     fmt.Println(req)
 
-    jobs <- *req
+    jobs <- req
 
-    backtopic := string(req.Uuid) + "-" + strconv.FormatInt(req.Epoch, 10) + "-data"
-    epoch := time.Now().UnixNano() / 1000000
-
-    datareq := DataRequest{
-        "dataRequest",
-        "abcdefg",
-        epoch,
-        backtopic}
-
-    reqjson, _ := json.Marshal(datareq)
-
-    mqttSubscribe(client, dataReceiveHandler, string(backtopic))
-    mqttPublish(client, "algorithm001-sensors", string(reqjson))
-
+    mqttSubscribe(client, WifiDataHandler, req.WifiTopic)
 }
 
 func mqttPublish(c *MQTT.MqttClient, topic string, message string) {
@@ -100,8 +99,6 @@ func mqttPublish(c *MQTT.MqttClient, topic string, message string) {
     <-receipt
 }
 
-//subscribe to the topic /go-mqtt/sample and request messages to be delivered
-//at a maximum qos of zero, wait for the receipt to confirm the subscription
 func mqttSubscribe(c *MQTT.MqttClient, handler MQTT.MessageHandler, topic string) {
     filter, e := MQTT.NewTopicFilter(topic, byte(MQTT.QOS_ZERO))
     if e != nil {
@@ -159,11 +156,7 @@ func main() {
         fmt.Printf("Connected to %s\n", "tcp://bearloc.cal-sdb.org:52411")
     }
 
-    //subscribe to the topic /go-mqtt/sample and request messages to be delivered
-    //at a maximum qos of zero, wait for the receipt to confirm the subscription
     mqttSubscribe(c, locRequestHandler, "algorithm001-request")
-
-    mqttSubscribe(c, f, "test")
 
     for {
         time.Sleep(1 * time.Second)
